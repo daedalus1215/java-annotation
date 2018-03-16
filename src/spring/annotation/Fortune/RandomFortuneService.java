2ce371b7f5876b1da87cae90c376a7e4552668f7
larry <ladams1776@yahoo.com>
2018-03-16 06:33:08 -0400

    adjustment to RandomFortuneService

--- src/spring/annotation/Fortune/RandomFortuneService.java
+++ src/spring/annotation/Fortune/RandomFortuneService.java
@@ -1,15 +1,29 @@
 package spring.annotation.Fortune;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneServiceInterface {	
+	
+	// create an array of string
+	private String[] data = {
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	};
+	
+	private Random randomGenerator = new Random();
 	
 	/* (non-Javadoc)
 	 * @see Fortune.FortuneServiceInterface#getFortune()
 	 */
 	@Override
 	public String getFortune() {
-		return "Fortune from the RandomFortuneService. With a fortune: " ;
+		// pick a random string from the array
+		int index  = (this.randomGenerator.nextInt(this.data.length));
+		String theFortune = this.data[index];
+		return "Fortune from the RandomFortuneService. With a fortune: " + theFortune;
 	}
 }
