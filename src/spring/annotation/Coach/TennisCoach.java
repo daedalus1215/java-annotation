4a29f78d2860e35119aeba7ecaf0dd7f17d26fdf
larry <ladams1776@yahoo.com>
2018-03-18 16:41:16 -0400

    finally got annotation to work, jar conflict aspect/context.

--- src/spring/annotation/Coach/TennisCoach.java
+++ src/spring/annotation/Coach/TennisCoach.java
@@ -4,18 +4,18 @@ package spring.annotation.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
+import org.springframework.context.annotation.Scope;
 import spring.annotation.Fortune.FortuneServiceInterface;
 
 
 @Component
 @Scope("prototype")
 public class TennisCoach implements CoachInterface {
+	
 	@Autowired
 	@Qualifier("randomFortuneService")
 	private FortuneServiceInterface fortuneService;
-	
-	
+		
 	@Override
 	public void displayDailyWorkout() {
 		System.out.println("Tennis coach's Daily workout!");
